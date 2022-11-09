56a8972e9f0741bf1095b799be34d991d3ac9918
Angelina <linaemalmros@gmail.com>
2022-11-09 12:20:20 +0100

    added entity and player classes

---
+++ src/entity/Entity.java
@@ -0,0 +1,10 @@
+package entity;
+
+// Stores variables that will be used in player, monster and NPC classes
+public class Entity {
+
+    public int x, y;
+    public int speed;
+
+
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable {
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
-    final int tileSize = originalTileSize * scale; // 16x3 = 48 -> 48x48 tile
+    public final int tileSize = originalTileSize * scale; // 16x3 = 48 -> 48x48 tile
     final int maxScreenCol = 18; // horizontally
     final int maxScreenRow = 14; // vertically
     final int screenWidth = tileSize * maxScreenCol;  // 864 pixels
@@ -17,6 +19,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     // PLAYER'S DEFAULT POSITION
     int playerX = 100;
@@ -60,27 +63,13 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        if (keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        if (keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }
-        if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();  // calling update in Player class
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.WHITE);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2); // calling draw in Player class
         g2.dispose();
     }
 }
